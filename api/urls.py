05c816e2d7a0a9d2a4f56172461505679bc408fe
Param Prabhakar <prabhakarparam@gmail.com>
2019-03-04 21:08:21 +0530

    added url routings

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('register', views.RegisterUser.as_view()),
+    path('login', views.LoginUser.as_view()),
+    path('seller_details', views.GetSellerDetails.as_view()),
+    path('place_bid', views.PlaceBid.as_view()),
+    path('rate_item', views.RateItem.as_view()),
+    path('item', views.Item.as_view()),
     path('', views.index),
 ]
